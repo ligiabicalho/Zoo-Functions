1d1b38119da7c27919cb48ecf9d22b85ec7c32b7
Lígia Bicalho <ligiabicalho@gmail.com>
2022-08-31 00:23:09 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const employeeObj = employees.find((employee) => employee.firstName === employeeName
+  || employee.lastName === employeeName);
+  return employeeObj || {};
 }
 
+console.log(getEmployeeByName('Bethea'));
+// firstName: 'Burl',
+// lastName: 'Bethea'
 module.exports = getEmployeeByName;
