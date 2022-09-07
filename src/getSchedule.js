51534414da2a379f94dbaf304d0aff3abf4bf97c
Lígia Bicalho <ligiabicalho@gmail.com>
2022-09-07 09:53:26 -0300

    Requisito 9!!!!!

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,49 +1,58 @@
 const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const availability = (scheduleTarget) =>
+const availabilityByAnimals = (scheduleTarget) =>
   species.find((specie) => scheduleTarget === specie.name).availability;
 
-const scheduleDay = (scheduleTarget) => {
+const scheduleByDay = (scheduleTarget) => {
   const day = hours[scheduleTarget];
   if (scheduleTarget === 'Monday') {
-    return 'Not open on Mondays';
+    return 'CLOSED';
   }
   return `Open from ${day.open}am until ${day.close}pm`;
 };
-console.log(scheduleDay('Sunday'));
+
+const animalsByDay = (scheduleTarget) => {
+  const animals = species.filter((specie) =>
+    specie.availability.includes(scheduleTarget));
+  const result = animals.reduce((acc, cur) => [...acc, cur.name], []);
+  if (result.length < 1) {
+    return 'The zoo will be closed!';
+  }
+  return result;
+};
+
+const allAnimals = species.reduce((acc, cur) => [...acc, cur.name], []);
+const allDays = Object.keys(hours);
+
+const buildOneDay = (scheduleTarget) => {
+  const objectDay = {};
+  objectDay[scheduleTarget] = {};
+  objectDay[scheduleTarget].officeHour = scheduleByDay(scheduleTarget);
+  objectDay[scheduleTarget].exhibition = animalsByDay(scheduleTarget);
+  return objectDay;
+};
+
+const allInformation = () => {
+  const result = {};
+  allDays.forEach((day) => {
+    const objectDay = {
+      officeHour: scheduleByDay(day),
+      exhibition: animalsByDay(day),
+    };
+    result[day] = objectDay;
+  });
+  return result;
+};
 
 function getSchedule(scheduleTarget) {
-  availability(scheduleTarget);
-  scheduleDay(scheduleTarget);
-  // return;
+  if (allAnimals.includes(scheduleTarget)) {
+    return availabilityByAnimals(scheduleTarget);
+  }
+  if (hours[scheduleTarget]) {
+    return buildOneDay(scheduleTarget);
+  }
+  return allInformation();
 }
-
-// console.log((getSchedule('animal'))); // Retorna: [ 'Tuesday', 'Thursday', 'Saturday', 'Sunday' ];
-// console.log((getSchedule('day'))); // Retorne os horários para aquele dia e quais animais estarão disponíveis.
-// console.log((getSchedule())); // Retorne todos os horários disponíveis para cada dia da semana
-// console.log((getSchedule(!animal, !day))); // Retorne todos os horários disponíveis para cada dia da semana
-
-// {
-//   Tuesday: { // Dia da semana
-//     officeHour: 'Open from 8am until 6pm', // n
-//     exhibition: [ 'lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes' ],
-//   },
-//   Wednesday: {
-//     officeHour: 'Open from 8am until 6pm',
-//     exhibition: [ 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes' ],
-//   },
-//   // [...]
-// }
-
-// hours: {
-//   Tuesday: { open: 8, close: 6 },
-//   Wednesday: { open: 8, close: 6 },
-//   Thursday: { open: 10, close: 8 },
-//   Friday: { open: 10, close: 8 },
-//   Saturday: { open: 8, close: 10 },
-//   Sunday: { open: 8, close: 8 },
-//   Monday: { open: 0, close: 0 },
-// },
 
 module.exports = getSchedule;
