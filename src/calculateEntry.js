184c6f7a3f0e60169b4f5aa436af575339503d58
Lígia Bicalho <ligiabicalho@gmail.com>
2023-03-30 09:42:50 -0300

    fix

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -28,4 +28,15 @@ function calculateEntry(entrants) {
   return result;
 }
 
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+calculateEntry(entrants);
+
 module.exports = { calculateEntry, countEntrants };
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,24 +2,16 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const allLocations = species.reduce((acc, cur) => {
-  if (!acc.includes(cur.location)) {
+  if (!acc[cur.location]) {
     return [...acc, cur.location];
   }
   return acc;
 }, []);
-
-const getLocation = () => {
-  const objectLocation = {};
-  allLocations.forEach((location) => objectLocation[location] = species.filter((specie) => 
-      specie.location === location).reduce((acc, cur) => [...acc, cur.name], []));
-  return objectLocation;
-};
-console.log(getLocation());
+console.log(allLocations());
 
 function getAnimalMap(options) {
-  if (typeof options === 'undefined' || Object.keys(options).length < 1) {
-    return  // console.log(objectLocation);
-  }
+  // TO-DO
+  // Terminar implementação!
 }
 getAnimalMap();
 
